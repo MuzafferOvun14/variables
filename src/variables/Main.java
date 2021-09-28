40483c311a1e01608a1e789831102c0950af76ab
MuzafferOvun14 <ibmtalbilisim@gmail.com>
2021-09-28 15:43:46 +0300

    Initial commit

---
+++ src/variables/Main.java
@@ -0,0 +1,15 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String studentName="Ahmet";
+		int studentAge=23;
+		System.out.println(studentName);
+		studentName="Emrah";
+		System.out.println(studentAge);
+		System.out.println(studentName+" yaþ "+studentAge);
+	}
+
+}
